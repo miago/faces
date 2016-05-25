febd5748d8405ba846385dd7aefd29a1f2eb42c2
Mirco Gysin <miagox@gmail.com>
2016-05-25 17:03:37 +0200

    added brain script

--- brain.m
+++ brain.m
@@ -5,8 +5,30 @@ load('background_data.mat');
 or_images = getORimages();
 
 
+
 for idx = 1:length(or_images)
-    % precalculate background locations
+    
+    current_background = background{idx};
+    current_image = or_images{idx};
+    
+    c_w = 4.5;
+    tmp_w = 5;
+    tex_w = 5;
+    last_w = 6;
+    
+    color_probability_map = getColorProbability(current_image, current_background);
+    %color_probability_map = colorProbMap{idx};
+    templates_probabolity_map = getTemplateProbability(current_image, current_background);
+    texture_probability_map = getTextureProbability(current_image, current_background);
+    if idx == 1
+        BGsubtraction(current_image, zeros(720, 1280));
+    else
+        BGsubtraction(current_image,or_images{idx-1});
+    end
+    last_position_probability_map = getLastPositionProbability(old_locations, scene_change);
+    
+    total_prob_map = c_w*color_probability_map + tmp_w * templates_probabolity_map + tex_w * texture_probability_map + last_w * last_position_probability_map;
+    total_prob_map = total_prob_map/(c_w+tmp_w+tex_w+last_w);
     
     sprintf('image number %d', idx)
 end
---
+++ getProbabilityMap.m
@@ -0,0 +1,32 @@
+function [ probability_map ] = getProbabilityMap( locations, picture_size, sigma)
+
+probability_map = zeros(picture_size(1), picture_size(2));
+
+region_size = 401;
+x=1:region_size;
+y=(1:region_size)';               
+[X,Y]=meshgrid(x,y);
+
+z=exp(-((X-region_size/2).^2+(Y-region_size/2).^2)/(2*pi*sigma^2))/(2*pi*sigma^2);
+
+for person = fieldnames(locations)'
+    x_coor = locations.(person{1}).x;
+    y_coor = locations.(person{1}).y;
+    
+    cropped_exp = z(max(1,(region_size+1)/2-y_coor+1):min(region_size, picture_size(1)-y_coor+(region_size+1)/2),max(1,(region_size+1)/2-x_coor+1):min(region_size, picture_size(2)-x_coor+(region_size+1)/2));
+    
+    x_start = max(1,x_coor-(region_size-1)/2);
+    x_stop = min(picture_size(2),x_coor+(region_size-1)/2);
+    y_start = max(1,y_coor-(region_size-1)/2);
+    y_stop = min(picture_size(1),y_coor+(region_size-1)/2);
+   
+    probability_map(y_start:y_stop, x_start:x_stop) = ...
+        probability_map(y_start:y_stop, x_start:x_stop) + cropped_exp;  
+end
+
+figure();
+
+surf(1:picture_size(2),1:picture_size(1),probability_map);shading interp
+
+end
+
